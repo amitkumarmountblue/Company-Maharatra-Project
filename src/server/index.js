0e92ab76bd0e17467c616d4eda2e230d6a90df82
amitkumarmountblue <amit.kumar.17@mountblue.tech>
2021-08-26 14:38:44 +0530

    Added

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,34 @@
-const csv = require('csv-parser');
-const fs = require('fs');
-const {authorizeCap, dateOfRegistration,registrationPrincipalBusiness,registrationIndustrialClass}=require('./problem');
-const maharastraData=[];
+const csv = require("csv-parser");
+const fs = require("fs");
+const {
+  authorizeCap,
+  dateOfRegistration,
+  registrationPrincipalBusiness,
+  registrationIndustrialClass,
+} = require("./problem");
+const maharastraData = [];
 
-fs.createReadStream('../Data/Maharastra.csv')
-    .pipe(csv({}))  
-    .on('data', (data) => maharastraData.push(data))   
-    .on('end', () => {
-        const cap= authorizeCap(maharastraData);
-        const registration= dateOfRegistration(maharastraData);
-        const registrationPBA=registrationPrincipalBusiness(maharastraData);
-        const registrationIC=registrationIndustrialClass(maharastraData);
-      
-        function dumpData(data, file){
-            fs.writeFile(file, JSON.stringify(data,null,2), function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+fs.createReadStream("../Data/Maharastra.csv")
+  .pipe(csv({}))
+  .on("data", (data) => maharastraData.push(data))
+  .on("end", () => {
+    const cap = authorizeCap(maharastraData);
+    const registration = dateOfRegistration(maharastraData);
+    const registrationPBA = registrationPrincipalBusiness(maharastraData);
+    const registrationIC = registrationIndustrialClass(maharastraData);
+
+    function dumpData(data, file) {
+      fs.writeFile(file, JSON.stringify(data, null, 2), function (err) {
+        if (err) {
+          return console.log(err);
         }
-        dumpData(cap, '../Output/authorized_cap.json');
-        dumpData(registration,'../Output/date_of_registration.json');
-        dumpData(registrationPBA,'../Output/resgistration_Principal_Business.json');  
-        dumpData(registrationIC,'../Output/registration_industrial_class.json'); 
-    });
+      });
+    }
+    dumpData(cap, "../Output/authorized_cap.json");
+    dumpData(registration, "../Output/date_of_registration.json");
+    dumpData(
+      registrationPBA,
+      "../Output/resgistration_Principal_Business.json"
+    );
+    dumpData(registrationIC, "../Output/registration_industrial_class.json");
+  });
--- src/server/problem.js
+++ src/server/problem.js
@@ -1,96 +1,96 @@
-function authorizeCap(maharastraData){
-    const result={};
-    let key;
-    for(data of maharastraData){
-        if(data.AUTHORIZED_CAP > 0 && data.AUTHORIZED_CAP<100000){
-            key= 1;
-            if(key in result){
-                result[key]+=1;
-            }else{
-                result[key]=1;
-            }
-        }else if(data.AUTHORIZED_CAP >= 100000 && data.AUTHORIZED_CAP < 1000000){
-            key= 2;
-            if(key in result){
-                result[key]+=1;
-            }else{
-                result[key]=1;
-            }
-        }else if(data.AUTHORIZED_CAP >= 1000000 && data.AUTHORIZED_CAP < 10000000){
-            key= 3;
-            if(key in result){
-                result[key]+=1;
-            }else{
-                result[key]=1;
-            }
-        }else if(data.AUTHORIZED_CAP >= 100000000 && data.AUTHORIZED_CAP < 1000000000){
-            key= 4;
-            if(key in result){
-                result[key]+=1;
-            }else{
-                result[key]=1;
-            }
-        }else{
-            key = 5;
-            if(key in result){
-                result[key]+=1;
-            }else{
-                result[key]=1;
-            }
-        }
+function authorizeCap(maharastraData) {
+  const result = {};
+  let key;
+  maharastraData.forEach((data) => {
+    if (data.AUTHORIZED_CAP > 0 && data.AUTHORIZED_CAP < 100000) {
+      key = "< 1L";
+    } else if (data.AUTHORIZED_CAP < 1000000) {
+      key = "1L to 10L";
+    } else if (data.AUTHORIZED_CAP < 10000000) {
+      key = "10L to 1Cr";
+    } else if (data.AUTHORIZED_CAP < 1000000000) {
+      key = "10Cr to 100Cr";
+    } else {
+      key = "More than 100Cr";
     }
-    return result;
+    if (key in result) {
+      result[key] += 1;
+    } else {
+      result[key] = 1;
+    }
+  });
+  return result;
 }
 
-
-function dateOfRegistration(maharastraData){
-    const result={};
-    for(let data of maharastraData){
-        const date=data.DATE_OF_REGISTRATION;
-        const year=date.slice(-2);
-        if(year in result){
-            result[year]+=1;
-        }else{
-            result[year]=1;
-        }
+function dateOfRegistration(maharastraData) {
+  const result = {};
+  maharastraData.forEach((data) => {
+    let date = data.DATE_OF_REGISTRATION;
+    let year = date.slice(-2);
+    if (year >= 00 && year < 22) {
+      year = "20" + year;
+    } else if (year <= 99) {
+      year = "19" + year;
+    } else {
+      year = year;
     }
-    return result;
+    if (year in result) {
+      result[year] += 1;
+    } else {
+      result[year] = 1;
+    }
+  });
+  return result;
 }
 
-function registrationPrincipalBusiness(maharastraData){
-    let result={};
-    for(let data of maharastraData){
-        const date=data.DATE_OF_REGISTRATION;
-        const year=date.slice(-2);
-        if(year=='15'){
-            const type=data.PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN;
-            if(type in result){
-                result[type]+=1;
-            }else{
-                result[type]=1;
-            }
-        }
+function registrationPrincipalBusiness(maharastraData) {
+  let result = {};
+  maharastraData.forEach((data) => {
+    const date = data.DATE_OF_REGISTRATION;
+    const year = date.slice(-2);
+    if (year == "15") {
+      const type = data.PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN;
+      if (type in result) {
+        result[type] += 1;
+      } else {
+        result[type] = 1;
+      }
     }
-    result = Object.entries(result).sort((a,b) => b[1]-a[1]).slice(0,10);
-    return result;
+  });
+  result = Object.entries(result)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10);
+  const output = result.map(([PRINCIPAL_BUSINESS_ACTIVITY, Number]) => ({
+    PRINCIPAL_BUSINESS_ACTIVITY,
+    Number,
+  }));
+
+  return output;
 }
 
-function registrationIndustrialClass(maharastraData){
-    let result={};
-    for(let data of maharastraData){
-        const industrialClass=data.Industrial_Class;
-        if(industrialClass in result){
-            result[industrialClass]+=1;
-        }else{
-            result[industrialClass]=1;
-        }
+function registrationIndustrialClass(maharastraData) {
+  let result = {};
+  maharastraData.forEach((data) => {
+    const industrialClass = data.Industrial_Class;
+    if (industrialClass in result) {
+      result[industrialClass] += 1;
+    } else {
+      result[industrialClass] = 1;
     }
-    result=Object.entries(result).sort((a,b) => b[1]-a[1]).slice(0,10);
+  });
+  result = Object.entries(result)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10);
 
-    return result;
+  const output = result.map(([Industrial_Class, Number]) => ({
+    Industrial_Class,
+    Number,
+  }));
+
+  return output;
 }
 
-module.exports.authorizeCap=authorizeCap;
-module.exports.dateOfRegistration=dateOfRegistration;
-module.exports.registrationPrincipalBusiness=registrationPrincipalBusiness;
-module.exports.registrationIndustrialClass=registrationIndustrialClass;
+module.exports.authorizeCap = authorizeCap;
+module.exports.dateOfRegistration = dateOfRegistration;
+module.exports.registrationPrincipalBusiness = registrationPrincipalBusiness;
+module.exports.registrationIndustrialClass = registrationIndustrialClass;
